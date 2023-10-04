3c4662e59c41e4c4e8bbe477278ade0ef564116f
DongWooKim97 <nanaukim@naver.com>
2023-10-04 17:33:45 +0900

    fix: Open API에서 null값이 들어오는 경우 앱 정책으로 0으로 값을 저장하도록 변경

--- lib/model/stat_model.dart
+++ lib/model/stat_model.dart
@@ -42,25 +42,25 @@ class StatModel {
   //response값에서 우리가 원하는 값만 넣어주면 된다.
   // 개발자들의 약속임. json으로 부터 데이터를 받아서 이러한 형태를 형성할거면, 아래와 같이 사용해야한다.
   StatModel.fromJson({required Map<String, dynamic> json})
-      : daegu = double.parse(json['daegu']),
-        chungnam = double.parse(json['chungnam']),
-        incheon = double.parse(json['incheon']),
-        daejeon = double.parse(json['daejeon']),
-        gyeongbuk = double.parse(json['gyeongbuk']),
-        jeonbuk = double.parse(json['jeonbuk']),
-        sejong = double.parse(json['sejong']),
-        gwangju = double.parse(json['gwangju']),
-        gangwon = double.parse(json['gangwon']),
-        ulsan = double.parse(json['ulsan']),
-        jeonnam = double.parse(json['jeonnam']),
-        seoul = double.parse(json['seoul']),
-        busan = double.parse(json['busan']),
-        jeju = double.parse(json['jeju']),
-        chungbuk = double.parse(json['chungbuk']),
-        gyeongnam = double.parse(json['gyeongnam']),
+      : daegu = double.parse(json['daegu'] ?? '0'),
+        chungnam = double.parse(json['chungnam'] ?? '0'),
+        incheon = double.parse(json['incheon'] ?? '0'),
+        daejeon = double.parse(json['daejeon'] ?? '0'),
+        gyeongbuk = double.parse(json['gyeongbuk'] ?? '0'),
+        jeonbuk = double.parse(json['jeonbuk'] ?? '0'),
+        sejong = double.parse(json['sejong'] ?? '0'),
+        gwangju = double.parse(json['gwangju'] ?? '0'),
+        gangwon = double.parse(json['gangwon'] ?? '0'),
+        ulsan = double.parse(json['ulsan'] ?? '0'),
+        jeonnam = double.parse(json['jeonnam'] ?? '0'),
+        seoul = double.parse(json['seoul'] ?? '0'),
+        busan = double.parse(json['busan'] ?? '0'),
+        jeju = double.parse(json['jeju'] ?? '0'),
+        chungbuk = double.parse(json['chungbuk'] ?? '0'),
+        gyeongnam = double.parse(json['gyeongnam'] ?? '0'),
         dataTime = DateTime.parse(json['dataTime']),
         itemCode = parseItemCode(json['itemCode']),
-        gyeonggi = double.parse(json['gyeonggi']);
+        gyeonggi = double.parse(json['gyeonggi'] ?? '0');
 
   //ItemCode.NO2.toString() ->'ItemCode.NO2.toString()'
   //ItemCode.NO2.name => 'NO2'
@@ -71,6 +71,7 @@ class StatModel {
   }
 }
 
-
 // 왜 Constructor에 저런식으로 넣어줬냐?
 // 앞으로도 자주 사용되기 떄문에 ~~.fromJson이라고 작성하면 손쉽게 ~~클래스의 인스턴스를 만들 수 있기 때문이다.
+
+// 만약 null값이 들어오면 0이라고 가정하자(앱 별로 정책이 다름) -> 우리가 정하는것.

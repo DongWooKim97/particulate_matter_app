4b5102ceac3be7b22c56b1e23552edd86b77e7f8
DongWooKim97 <nanaukim@naver.com>
2023-10-05 17:13:09 +0900

    chore: optimize imports

--- android/app/src/main/kotlin/com/particulate_matter/particulate_matter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/particulate_matter/particulate_matter_app/MainActivity.kt
@@ -2,5 +2,5 @@ package com.particulate_matter.particulate_matter_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
